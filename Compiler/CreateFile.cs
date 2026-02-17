1ee3b3ee4b2b474b23c6ce00206eb514ab03f437
LWhite200 <157498802+LWhite200@users.noreply.github.com>
2026-02-17 09:46:12 -0500

    Add files via upload

---
+++ Compiler/CreateFile.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace Compiler
+{
+    internal class CreateFile
+    {
+     
+        /*
+        PH<reg>: push the contents of the register(i.e.A, X, or Y) onto the stack.
+        PL<reg>: pop a value off the stack into the register.The program will terminate if this instruction is called when the stack is empty.
+        AD: pop two values off the stack, and push the lowest bits of their sum onto the stack.
+        ZE<reg>: set the register equal to zero.
+        ST<reg>: set the register equal to one.
+        DI<reg>: send the value of the register to the laser display board and exit.
+        */
+
+
+        public static void FileCreationMain()
+        {
+            Console.WriteLine("Input Number 0-255");
+            string? n = Console.ReadLine();
+
+            // Validate it is a valid number AND within range
+            if (!int.TryParse(n, out int number) || number < 0 || number > 255)
+            {
+                Console.WriteLine("Invalid Input");
+                return;
+            }
+
+            Console.WriteLine("Valid number: " + number);
+
+
+
+
+
+        }
+    }
+}
---
+++ Compiler/Program.cs
@@ -0,0 +1,42 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Compiler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ask user
+            Console.WriteLine("Welcome To Compiler!\n");
+            Console.WriteLine("0: Create File\n");
+            Console.WriteLine("1: Read File\n");
+
+            string? input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                // null OR ""
+                Console.WriteLine("Invalid Input\n");
+                Console.WriteLine("Closing Program...\n");
+                input = Console.ReadLine();
+                return;
+            }
+
+            // --==--==--==--==--==
+            // Let's just create a file for Now ---
+
+            if (input == "0")
+            {
+                // CreateFile.FileCreationMain();
+            }
+            else if (input == "1")
+            {
+                // ReadFile.FileReadMain();
+            }
+
+            CreateFile.FileCreationMain();
+
+
+        }
+    }
+}
